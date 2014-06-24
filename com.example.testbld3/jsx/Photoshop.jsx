40ebc41db38a820fc15f374a91c1aecb4263a0a1
studioxop <contact@studioxop.com>
2014-06-24 12:51:33 +0200

    Create Photoshop.jsx

---
+++ com.example.testbld3/jsx/Photoshop.jsx
@@ -0,0 +1,27 @@
+$._ext_BTN01={
+    run : function() {
+    
+    	/**********  Replace below sample code with your own JSX code  **********/
+        var appName;	    
+	    appName = "BTN01";	    
+        alert(appName);
+        app.doAction( 'test', 'test' );
+        /************************************************************************/
+        
+        return appName;
+    },
+};
+
+$._ext_BTN02={
+    run : function() {
+    
+    	/**********  Replace below sample code with your own JSX code  **********/
+        var appName;	    
+	    appName = "BTN02";	    
+        alert(appName);
+        app.doAction( 'test', 'test2' );
+        /************************************************************************/
+        
+        return appName;
+    },
+};
